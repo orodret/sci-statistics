64d1f18602541a56161451433d9416ab0956e3f2
Kirill <kirantonez@gmail.com>
2015-11-15 14:40:23 +0300

    Add index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mean: require('./lib/mean.js'),
+    median: require('./lib/median.js'),
+    number: require('./lib/number.js'),
+    percentile: require('./lib/percentile.js'),
+    standardDeviation: require('./lib/standarddeviation.js'),
+    sum: require('./lib/sum.js'),
+    variance: require('./lib/variance.js')
+};
