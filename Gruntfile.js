d8e4a456a5b88db0b6c78050a8ea474853c3a413
Kirill <kirantonez@gmail.com>
2015-11-15 18:09:36 +0300

    Add grunt to test

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        jasmine_node: {
+            test: {
+                options: {
+                    coverage: {},
+                    forceExit: true,
+                    match: '.',
+                    matchAll: false,
+                    specFolders: ['test'],
+                    extensions: 'js',
+                    specNameMatcher: 'spec',
+                    captureExceptions: true,
+                    junitreport: {
+                        report: false,
+                        savePath: './build/reports/jasmine',
+                        useDotNotation: true,
+                        consolidate: true
+                    }
+                },
+                src: ['**/*.js']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-jasmine-node-coverage');
+
+    grunt.registerTask('default', 'jasmine_node');
+};
